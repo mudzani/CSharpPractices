ba8e598bc860356092b4025b4232df494216212e
Donald Muzani <donald.mudzani@gmail.com>
2026-07-09 01:15:14 +0200

    Add user input prompts and simple calculator feature
    Expanded the program to prompt for user details (name, age, height, residency, net worth, LinkedIn) and display them. Added a calculator that sums and divides two user-provided numbers. Improved output formatting.

--- BasicVariables/Program.cs
+++ BasicVariables/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace BasicVariables
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace BasicVariables
 {
     public class Program
     {
@@ -23,7 +25,45 @@
             //different formatting options
             Console.WriteLine("My name is {0}, I am {1} years old, and my estimated net worth is ${2}.", myName, myAge, myEstimatedNetWorth);
             Console.WriteLine("my Height is {0}", myHeight);
-            Console.WriteLine("my name " + myName); 
+            Console.WriteLine("my name " + myName);
+            Console.WriteLine($"once again my Linkedin Link is {myLinkedin}");
+
+            //prompt the user to enter
+            Console.Write("Please enter your name :");
+            string nameInput = Console.ReadLine();
+            Console.Write("please enter your age :");
+            int ageInput = int.Parse(Console.ReadLine());
+            Console.Write("please enter your height in metres:");
+            double heihtInput = double.Parse(Console.ReadLine());
+            Console.Write("Are you are permenat citizen? Enter True or False :"); 
+            bool permenantInput = bool.Parse (Console.ReadLine());
+            Console.Write("Please enter your estimated net worth :");
+            double networthInput = double.Parse(Console.ReadLine());
+            Console.Write("please enter your Linkedin link :");
+            string linkedinInput = Console.ReadLine();
+
+            // Display the user input
+            Console.WriteLine("my name  is " + nameInput);
+            Console.WriteLine("i am " + ageInput + " years old");
+            Console.WriteLine("i am " + heihtInput + " metres tall");
+            Console.WriteLine("permenat resident? " + permenantInput );
+            Console.WriteLine("my estimated networth is: " + networthInput);
+            Console.WriteLine("my Linkedin Link is : " + linkedinInput);
+
+
+            Console.WriteLine("TypeAndCastCalculator");
+            Console.WriteLine("Please enter first number  :");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter second to add :");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int sum = number1 + number2;
+            double quotiet = (double) number1 / number2;
+
+            Console.WriteLine($"{number1} + {number2} = {sum}");
+            Console.WriteLine($"{number1} / {number2} = {quotiet}");
+
+
 
 
 
