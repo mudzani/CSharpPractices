857acfde7c52ac53371b47f1075c5c747cf4902b
Donald Muzani <donald.mudzani@gmail.com>
2026-07-09 15:26:55 +0200

    Fix variable name typos and remove redundant output
    Corrected multiple spelling errors in variable names for consistency (`isPermenentResident`, `heihtInput`, `permenantInput`, `networthInput`). Updated all references to use the corrected names. Removed a redundant line displaying the user's name.

--- BasicVariables/Program.cs
+++ BasicVariables/Program.cs
@@ -9,7 +9,7 @@ namespace BasicVariables
             // Declare variables of different data types
             int myAge = 20;
             double myHeight = 1.75;
-            bool isPermenentResident = true;
+            bool isPermanentResident = true;
             string myName = "John Doe";
             double myEstimatedNetWorth = 1000000.50;
             string myLinkedin = "https://www.linkedin.com/in/johndoe";
@@ -18,14 +18,13 @@ namespace BasicVariables
             Console.WriteLine("My name is " + myName);
             Console.WriteLine("I am " + myAge + " years old.");
             Console.WriteLine("I am " + myHeight + " meters tall.");
-            Console.WriteLine("Permanent resident? " + isPermenentResident);
+            Console.WriteLine("Permanent resident? " + isPermanentResident);
             Console.WriteLine("My estimated net worth is $" + myEstimatedNetWorth);
             Console.WriteLine("My LinkedIn link is: " + myLinkedin);
 
             //different formatting options
             Console.WriteLine("My name is {0}, I am {1} years old, and my estimated net worth is ${2}.", myName, myAge, myEstimatedNetWorth);
             Console.WriteLine("my Height is {0}", myHeight);
-            Console.WriteLine("my name " + myName);
             Console.WriteLine($"once again my Linkedin Link is {myLinkedin}");
 
             //prompt the user to enter
@@ -34,20 +33,20 @@ namespace BasicVariables
             Console.Write("please enter your age :");
             int ageInput = int.Parse(Console.ReadLine());
             Console.Write("please enter your height in metres:");
-            double heihtInput = double.Parse(Console.ReadLine());
+            double heightInput = double.Parse(Console.ReadLine());
             Console.Write("Are you are permenat citizen? Enter True or False :"); 
-            bool permenantInput = bool.Parse (Console.ReadLine());
+            bool permanentInput = bool.Parse(Console.ReadLine());
             Console.Write("Please enter your estimated net worth :");
-            double networthInput = double.Parse(Console.ReadLine());
+            double netWorthInput = double.Parse(Console.ReadLine());
             Console.Write("please enter your Linkedin link :");
             string linkedinInput = Console.ReadLine();
 
             // Display the user input
             Console.WriteLine("my name  is " + nameInput);
             Console.WriteLine("i am " + ageInput + " years old");
-            Console.WriteLine("i am " + heihtInput + " metres tall");
-            Console.WriteLine("permenat resident? " + permenantInput );
-            Console.WriteLine("my estimated networth is: " + networthInput);
+            Console.WriteLine("i am " + heightInput + " metres tall");
+            Console.WriteLine("permenat resident? " + permanentInput);
+            Console.WriteLine("my estimated networth is: " + netWorthInput);
             Console.WriteLine("my Linkedin Link is : " + linkedinInput);
 
 
