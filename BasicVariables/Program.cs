f6f1a1fa4b62f7fe85fa553d816a9f021d18a54c
Donald Muzani <donald.mudzani@gmail.com>
2026-07-13 15:20:52 +0200

    project setup

---
+++ BasicVariables/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicVariables
+{
+    internal class Program
+    {
+    }
+}
---
+++ BooleanVote/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BooleanVote
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
