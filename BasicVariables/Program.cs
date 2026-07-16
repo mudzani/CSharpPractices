13bd839f3df43569af6449949c88c9020be8a35e
Donald Muzani <donald.mudzani@gmail.com>
2026-07-16 17:58:49 +0200

    Update capitalization in net worth output message
    Changed "My estimated net worth is:" to "My Estimated net worth is:" in the Console.WriteLine statement to standardize capitalization.

--- BasicVariables/Program.cs
+++ BasicVariables/Program.cs
@@ -23,8 +23,9 @@ namespace BasicVariables
             Console.WriteLine("I am "+ myAge+"years ");
             Console.WriteLine("I am " + myHeight + "m tall");
             Console.WriteLine("I am a permanent resident: " + isPermanentResident);
-            Console.WriteLine("My estimated net worth is: " + myEstimatedNetWorth);
+            Console.WriteLine("My Estimated net worth is: " + myEstimatedNetWorth);
             Console.WriteLine("My LinkedIn profile is: " + myLinkedin);
+
 
 
         }
