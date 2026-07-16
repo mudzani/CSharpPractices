bd01b09b7dadc097233e865b5aad90011b64d491
Donald Muzani <donald.mudzani@gmail.com>
2026-07-16 17:56:55 +0200

    Refactor variable declarations to use 'var' keyword
    Replaced explicit type declarations with 'var' in the Main method to enable type inference and improve code readability. No changes to program behavior.

--- BasicVariables/Program.cs
+++ BasicVariables/Program.cs
@@ -11,12 +11,20 @@ namespace BasicVariables
             Console.WriteLine("Hello, World!");
             Console.WriteLine("");
             // Declare a variable to hold the user's name
-            int myAge = 20;
-            double myHeight = 1.75;
-            bool isPermanentResident = true;
-            string myName = "Donald ";
-            double myEstimatedNetWorth = 1000000.50;
-            string myLinkedin = "https://www.linkedin.com/in/Donald";
+            var myAge = 20;
+            var myHeight = 1.75;
+            var isPermanentResident = true;
+            var myName = "Donald ";
+            var myEstimatedNetWorth = 1000000.50;
+            var myLinkedin = "https://www.linkedin.com/in/Donald";
+
+            // Print the user's name to the console
+            Console.WriteLine("my name is " + myName);
+            Console.WriteLine("I am "+ myAge+"years ");
+            Console.WriteLine("I am " + myHeight + "m tall");
+            Console.WriteLine("I am a permanent resident: " + isPermanentResident);
+            Console.WriteLine("My estimated net worth is: " + myEstimatedNetWorth);
+            Console.WriteLine("My LinkedIn profile is: " + myLinkedin);
 
 
         }
