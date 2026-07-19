531f0709d2160b136ad950fdafe4d50a8f4f4311
Donald Muzani <donald.mudzani@gmail.com>
2026-07-20 01:06:04 +0200

    Demonstrate use of constants for area and time calculations
    Added PI and C as constants. Program now prompts for radius to compute circle area and for distance to compute travel time at the speed of light. Updated output messages and greeting.

--- ConstantVariables/Program.cs
+++ ConstantVariables/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world this is CHashTag!"); 
+            Console.WriteLine("ConstantVariables!");
+            Console.WriteLine("");
+
+            const double PI = 3.1415926536;
+            const double C = 299792458;
+
+            double radius, distance;
+
+            Console.WriteLine("Please enter the radius in m to calculate area");
+            radius = double.Parse(Console.ReadLine());
+            double area = PI * radius * radius;
+            Console.WriteLine($"The area of a circle of radius {radius}m is {area}m^2");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter the distance in m to calculate time");
+            distance = double.Parse(Console.ReadLine());
+            double time = distance / C;
+            Console.WriteLine($"Takes {time} seconds to travel {distance}m at the speed of light C = {C}m/s");
         }
     }
 }
